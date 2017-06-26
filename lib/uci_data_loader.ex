6381eef8e5d6bf9edbcffdd5459a0427d98ff769
kokoax <yomo4808@gmail.com>
2017-06-26 15:38:45 +0900

    add document

--- lib/uci_data_loader.ex
+++ lib/uci_data_loader.ex
@@ -1,12 +1,45 @@
 defmodule UCIDataLoader do
   defstruct [:data, :target_all_name, :target_names, :length, :amount, :cluster_num, :each_amount]
+
+  @moduledoc """
+  UCI module has method that load of data from UCI repository.
+  ```
+  iex> UCIDataLoader.load_iris
+  %UCIDataLoader {
+    data:  [[5.1, 3.5, 1.4, 0.2], [4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2], ...],
+    target_all_name: ["Iris-setosa", "Iris-setosa", "Iris-setosa", "Iris-setosa", ...],
+    target_names: ["Iris-setosa", "Iris-versicolor", "Iris-virginica"],
+    length: 4,
+    amount: 150,
+    cluster_num: 3,
+    each_amount: [50, 50, 50],
+  }
+  ```
+  """
+
+  @doc """
+  Data load from iris in UCI repository.
+
+  ### refer
+  http://archive.ics.uci.edu/ml/datasets/Iris
+  """
   def load_iris do
     Iris.load
   end
+
+  @doc """
+  Data load from wine quality in UCI repository.
+
+  ### refer
+  http://archive.ics.uci.edu/ml/datasets/Wine+Quality
+  """
   def load_wine_quality do
     WineQuality.load
   end
 
+  @doc """
+  This method do number of n Sampling with Replacement.
+  """
   def sampling_with_replace(datasets, n) do
     rands = 1..n |> Enum.map(
       fn(_) ->
